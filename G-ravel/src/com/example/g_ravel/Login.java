aa08286d861f84d418f457878e549eda24f44a75
Administrator <=>
2014-10-01 17:58:36 +0900

    XML이 아직 한참 남았다.... 갈길이 멀어.. 어떡하지....

--- G-ravel/src/com/example/g_ravel/Login.java
+++ G-ravel/src/com/example/g_ravel/Login.java
@@ -1,5 +1,14 @@
 package com.example.g_ravel;
 
-public class Login {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class Login extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
 }
